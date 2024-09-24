9f7b6654b857f6a417a7841dd7901a3a16c938dd
SkynetHD <Madsmads86@hotmail.com>
2024-09-24 10:10:57 +0200

    Initial commit

---
+++ src/main/java/org/example/giithubtest/HelloGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.giithubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGithubActionsApplication.class, args);
+    }
+
+}
